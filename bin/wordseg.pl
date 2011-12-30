c9241aea117c9bbe0bca72fe7b9f96d9dcbabe17
raochenlin <rao.chenlin@gmail.com>
2011-12-30 16:20:02 +0800

    missing concatenation operators and config.yml

--- bin/wordseg.pl
+++ bin/wordseg.pl
@@ -7,6 +7,7 @@ use DBD::mysql;
 use Redis;
 
 #####Poem In
+my ($type, $title, $author, $content) = @ARGV;
 my $dbh = DBI->connect('DBI:mysql:poem:localhost', 'poem', 'poem', {RaiseError => 1});
 my $create_sth = $dbh->prepare('insert into poem (type, title, author, content) value (?,?,?,?)');
 $create_sth->execute($type, $title, $author, $content);
@@ -21,15 +22,15 @@ my $redis = Redis->new( server => '127.0.0.1:6379' );
 $redis->sadd($article_id, $_) for @words_array;
 foreach (1 .. $article_id - 1 ) {
     my @sinter = $redis->sinter($_, $article_id);
-    next unless define @sinter;
+    next unless @sinter;
     my @relate;
     push @relate, [$_, $#sinter] unless $#sinter < $relate[-1]->[1];
     shift @relate if $#relate > 9;
-    grep { $redis->sadd('relate:'$article_id, $_->[0]) } @relate if $_ + 1 == $article_id;
+    grep { $redis->sadd('relate:'.$article_id, $_->[0]) } @relate if $_ + 1 == $article_id;
 };
 
 #####Poem Out
-my $rand_id = $redis->srandmember('relate:'$article_id);
+my $rand_id = $redis->srandmember('relate:'.$article_id);
 my $rand_sth = $dbh->prepare('select * from poem where id = ?');
 $rand_sth->execute($rand_id);
 my @rand_article = $rand_sth->fetchrow_array;
--- lib/PoemRelate.pm
+++ lib/PoemRelate.pm
@@ -6,7 +6,7 @@ use Dancer::Plugin::Database;
 our $VERSION = '0.1';
 
 get '/' => sub {
-    my $sth = database->prepare('select author name, count(author) count from poem group by author');
+    my $sth = database('poem')->prepare('select author name, count(author) count from poem group by author');
     $sth->execute();
     my @index;
     my $i = 0;
@@ -20,7 +20,7 @@ get '/' => sub {
 };
 
 get '/list/:author' => sub {
-    my $sth = database->prepare('select id, title from poem order by id where author = ?');
+    my $sth = database('poem')->prepare('select id, title from poem order by id where author = ?');
     $sth->execute(params->{'author'});
     my @titles;
     my $i = 0;
@@ -34,12 +34,12 @@ get '/list/:author' => sub {
 };
 
 get '/poem/:titleid' => sub {
-    my $sth = database->prepare('select title, author, content from poem where id = ?');
+    my $sth = database('poem')->prepare('select title, author, content from poem where id = ?');
     $sth->execute(params->{'titleid'});
-    $mainref = $sth->fetchrow_hashref;
-    my $relate_id = redis->srandmember('relate:'params->{'titleid'});
+    my $mainref = $sth->fetchrow_hashref;
+    my $relate_id = redis->srandmember('relate:'.params->{'titleid'});
     $sth->execute($relate_id);
-    $relateref = $sth->fetchrow_hashref;
+    my $relateref = $sth->fetchrow_hashref;
     template 'poem', { main => $mainref, relate => $relateref };
 };
 
